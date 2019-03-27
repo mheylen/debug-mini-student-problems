91786ad7293e9a18f880c9ad42ca038ffc2ac8b5
Your Name <you@example.com>
2019-03-27 11:31:23 -0700

    added the /api on the app.get

--- server/index.js
+++ server/index.js
@@ -5,9 +5,10 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/data', (req, res) => {
+app.get('/api/data', (req, res) => {
   res.json({ greeting: 'hello' });
 });
+
 
 const SERVER_PORT = process.env.SERVER_PORT || 4000;
 app.listen(SERVER_PORT, () => {
